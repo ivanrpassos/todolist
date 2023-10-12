2d8c1a4f6fbecccd37ec741bd543a896faf118c5
Ivan Passos <ivanpassosfilho@gmail.com>
2023-10-12 14:06:37 -0300

    Initial Commit

---
+++ src/test/java/br/com/ivanrpassos/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ivanrpassos.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
